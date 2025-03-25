5199dc1cce5d9ab05c0cda84be2ac81cdf6a9456
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-03-25 16:45:08 -0400

    feat: Add the possibility to specify only the environment to use, and the tracking function will provide the correct URL (#8)

---
+++ packages/mixpanel/src/env.ts
@@ -0,0 +1,27 @@
+
+const EnvironmentList = ["development", "staging", "production", "local", "msw"] as const;
+export type Environment = typeof EnvironmentList[number];
+
+const NavigationApiBaseUrl: Record<Environment, string> = {
+    "production": "https://api.platform.workleap.com/shell/navigation/",
+    "staging": "https://api.platform.workleap-stg.com/shell/navigation/",
+    "development": "https://api.platform.workleap-dev.com/shell/navigation/",
+    "local": "https://api.platform.workleap-dev.com/shell/navigation/",
+    "msw": "/api/shell/navigation/"
+};
+
+// Cannot use URL() because it doesn't support relative base url: https://github.com/whatwg/url/issues/531.
+function resolveApiUrl(path: string, baseUrl: string | undefined) : string {
+    return `${baseUrl}${baseUrl!.endsWith("/") ? "" : "/"}${path.startsWith("/") ? path.substring(1) : path}`;
+}
+
+export function getTrackingEndpoint(envOrTrackingApiBaseUrl: Environment | (string & {})) {
+    let baseUrl: string;
+    if (EnvironmentList.includes(envOrTrackingApiBaseUrl as Environment)) {
+        baseUrl = NavigationApiBaseUrl[envOrTrackingApiBaseUrl as Environment];
+    } else {
+        baseUrl = envOrTrackingApiBaseUrl;
+    }
+
+    return resolveApiUrl("tracking/track", baseUrl);
+}
--- packages/mixpanel/src/index.ts
+++ packages/mixpanel/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./createTrackingFunction.ts";
+export type { Environment } from "./env.ts";
 export type { TrackEventProperties } from "./properties.ts";
 
