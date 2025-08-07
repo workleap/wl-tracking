09a52305d2aca44e6d292bacd647b479f2beda10
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-08-07 11:45:50 -0400

    fix typing

--- packages/mixpanel/src/js/context.ts
+++ packages/mixpanel/src/js/context.ts
@@ -4,7 +4,7 @@ export const ContextVariableName = "__WLP_MIXPANEL_CONTEXT__";
 
 export interface MixpanelContext {
     productId: string;
-    endpoint: string;
+    baseUrl: string;
     superProperties: SuperProperties;
     verbose: boolean;
 }
